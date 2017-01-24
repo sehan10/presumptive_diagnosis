3cc870e93c50ba15e889562544e0e22a433539fa
sehan <sehan_ahmed@live.com>
2017-01-24 19:22:21 +0500

    Fresh-5

--- userInformation/views.py
+++ userInformation/views.py
@@ -30,7 +30,7 @@ def decisionTree(request):
 # #Show response to DT #
 def responseTodt(request):
      instance = MethodRepository.MethodRepo.MethodCalling()
-    # accuracy,prediction = instance.response_ID3(request)
+     accuracy,prediction = instance.response_ID3(request)
      return render(request, 'UserInformation/Response.html', {'accuracy':accuracy, 'prediction':prediction})
 # #Show combo Form #
 def Combo(request):
